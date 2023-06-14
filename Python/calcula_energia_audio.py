ef4f84d49df9c6acc41185ea93919a2f0019671d
Gustavo-Arcary-Passos <117921675+Gustavo-Arcary-Passos@users.noreply.github.com>
2023-06-14 16:20:18 -0300

    Update calcula_energia_audio.py

--- Python/calcula_energia_audio.py
+++ Python/calcula_energia_audio.py
@@ -5,21 +5,21 @@ import time
 
 # Função para calcular a energia do áudio
 def calculate_audio_energy(audio_data):
-    energy = np.sum((np.abs(audio_data)*100) ** 2) / len(audio_data)
+    energy = np.sum((np.abs(audio_data)*100)**2) / len(audio_data)
     return energy
 
 # Função de callback para processar o áudio em tempo real
 def audio_callback(data,sample_rate,lista):
-    tempo = 0.05
+    tempo = 0.025
     tempo_max = librosa.get_duration(y=data, sr=sample_rate)
     while(tempo < tempo_max):
-        starter_time = tempo - 0.05  # Tempo de início em segundos
-        end_time = tempo + 0.05  # Tempo de fim em segundos
+        starter_time = tempo - 0.025  # Tempo de início em segundos
+        end_time = tempo + 0.025  # Tempo de fim em segundos
         # Converte o intervalo de tempo para amostras
         start_sample = int(starter_time * sample_rate)
         end_sample = int(end_time * sample_rate)
         lista.append(calculate_audio_energy(data[start_sample:end_sample]))
-        tempo += 0.1
+        tempo += 0.05
 
 # Carrega o arquivo de áudio
 def createEnergyList(nome_audio):
@@ -27,6 +27,9 @@ def createEnergyList(nome_audio):
     data, sample_rate = librosa.load(nome_audio)
     #r"C:\Users\micro1\Downloads\audio.wav"
     audio_callback(data,sample_rate,energiListy)
+    maior = max(energiListy)
+    for i in range(len(energiListy)):
+        energiListy[i] = energiListy[i]/maior * 100
     return energiListy
 
 #print(data[start_sample:end_sample])
